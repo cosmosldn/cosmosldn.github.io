df2d8c51fdb96a381958ec6441d0f84d06461d68
Henrique Alves <henriquea@gmail.com>
2015-08-21 13:21:58 +0100

    Fix react DOM warnings #4

--- src/components/split-screen/index.js
+++ src/components/split-screen/index.js
@@ -6,16 +6,15 @@ import marked from 'marked';
 export default React.createClass({
   render () {
     let data = this.props.data;
-    let items = data.products.map(function(item) {
+    let items = data.products.map(function(item, i) {
       let rawMarkup = marked(item.text.toString(), {sanitize: true});
       return (
-        <div className="SplitScreen-item">
+        <div className="SplitScreen-item" key={i}>
           <h3>{item.title}</h3>
           <p><span dangerouslySetInnerHTML={{__html: rawMarkup}} /></p>
         </div>
       )
     });
-
     return (
       <div className="SplitScreen SplitScreen--dark">
         <div className="Container">
