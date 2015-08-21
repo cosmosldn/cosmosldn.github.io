7449cc84d4c6163932e12087ff9fd8897337b57d
Henrique Alves <henriquea@gmail.com>
2015-08-21 10:27:30 +0100

    Add markdown support to text blocks

--- src/components/split-screen/index.js
+++ src/components/split-screen/index.js
@@ -1,16 +1,17 @@
 import './style.css';
 
 import React from 'react';
+import marked from 'marked';
 
 export default React.createClass({
   render () {
-    var data = this.props.data;
-
-    var items = data.products.map(function(item) {
+    let data = this.props.data;
+    let items = data.products.map(function(item) {
+      let rawMarkup = marked(item.text.toString(), {sanitize: true});
       return (
         <div className="SplitScreen-item">
           <h3>{item.title}</h3>
-          <p>{item.copy}</p>
+          <p><span dangerouslySetInnerHTML={{__html: rawMarkup}} /></p>
         </div>
       )
     });
