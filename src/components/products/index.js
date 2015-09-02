5ebb1fbf25229247a6ad3a5de5972cfa25306c46
Pedro Duarte <contact@peduarte.com>
2015-09-02 21:39:58 +0100

    Products hero image

--- src/components/products/index.js
+++ src/components/products/index.js
@@ -9,6 +9,16 @@ export default class Products extends Component {
 
     return (
       <div className={styles.root}>
+        <div className={styles.image}>
+          <div className={styles.prototype1}></div>
+          <div className={styles.prototype2}></div>
+          <div className={styles.prototype3}></div>
+          <div className={styles.styleguide1}></div>
+          <div className={styles.styleguide2}></div>
+          <div className={styles.styleguide3}></div>
+          <div className={styles.styleguide4}></div>
+          <div className={styles.styleguide5}></div>
+        </div>
         <SplitScreen data={data.products} />
       </div>
     );
